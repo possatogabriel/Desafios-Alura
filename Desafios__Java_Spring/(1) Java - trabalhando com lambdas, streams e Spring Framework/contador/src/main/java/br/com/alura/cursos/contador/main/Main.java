6cb85107d2ee3e954f61144edaa884665be8360d
possatogabriel <chaymitoyt@gmail.com>
2025-03-10 11:50:49 -0300

    Add exercises 3, 4 & 5 to 'Java Spring' challenges

--- Desafios__Java_Spring/(1) Java - trabalhando com lambdas, streams e Spring Framework/contador/src/main/java/br/com/alura/cursos/contador/main/Main.java
+++ Desafios__Java_Spring/(1) Java - trabalhando com lambdas, streams e Spring Framework/contador/src/main/java/br/com/alura/cursos/contador/main/Main.java
@@ -4,18 +4,25 @@ import java.util.Scanner;
 
 public class Main {
     Scanner input = new Scanner(System.in);
+    Tarefa classeTarefa = new Tarefa();
 
     public void rodaAplicacao() {
         rodaExercicio1();
+
+        classeTarefa.rodaExercicio4();
+
+        classeTarefa.rodaExercicio5();
     }
 
     private void rodaExercicio1() {
         // 1. Crie um novo projeto com Spring Boot, usando o site start.spring.io. Esse projeto será o contador, e funcionará na linha de comando. Você deve pedir para um usuário digitar um número e depois imprimir na tela uma contagem de 1 até o número digitado pelo usuário.
-        System.out.print("Digite um número: ");
+        System.out.print("(1) - Digite um número: ");
         var numero = input.nextInt();
 
         for (var i = 1; i <= numero; i++) {
-            System.out.println(i);
+            System.out.println("(1) - " + i);
         }
+
+        // VER 'pom.xml' PARA EXERCÍCIO 2
     }
 }
